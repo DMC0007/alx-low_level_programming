693061ea6b7dfd44ba1b895b012eec698d888a85
DMC0007 <wakataha@gmail.com>
2023-03-17 12:44:04 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Successs)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+
+	d = 'A';
+	while
+		(c <= 'z' && d <= 'Z') {
+			putchar(c);
+			c++;
+			puthcar(d);
+			d++;
+		}
+	return (0);
+}
