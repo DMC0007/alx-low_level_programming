23e24dab345c95304913f8f5df10b17009e1c506
DMC0007 <wakataha@gmail.com>
2023-03-23 03:58:29 -0700

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,33 +8,23 @@
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int num, mult, prod;
 
-	for (a = 0; a <= 9; a++)
+	for (num = 0; num <= 9; num++)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	c = a + b;
-	if (c >= 9)
-	{
-	d = c % 10;
-	e = (c - d) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(e + '0');
-	_putchar(d + '0');
-	}
-	else
-	{
-	if (b != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(c + '0');
-	}
-	}
-	_putchar('\n');
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult ++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod % 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
 	}
 }
