5abe79d9fe333aebd195fc2d690d49de5f3b0576
DMC0007 <wakataha@gmail.com>
2023-03-27 11:45:24 -0700

    Fixing unstaged & uncommited files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 -Takes a pointer to an int and updates the value it points tot to 98
+ * return_to_98 - Takes the pointer to an int as parameter and updates the value it points to to 98
  * @n: An integer input pointer
  * Return: nothing!
  */
 
-void return_to_98(int *n)
+void reset_to_98(int *n)
 {
-	*n = 98;
+	*n = 98
 }
