0086475f084c643e3e605cb7b6e7e0016d48ec6f
DMC0007 <wakataha@gmail.com>
2023-04-12 00:37:22 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,15 @@ include "main.h"
 
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
+	int i, n;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (k = 0; accept[k]; k++)
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (*s == accept[k])
-				return (s);
+			if (s[i] == accept[n])
+				return (s + i);
 		}
-		s++;
 	}
-	return ('\0');
-}
+	return (NULL);
+
